099dcff5d1d57c4ffffc1733b0ecefdc4b459632
nidakhan-01 <nidakhan8459@gmail.com>
2025-10-24 17:21:17 +0530

    Add files via upload

---
+++ Day 26/Q52.c
@@ -0,0 +1,26 @@
+/*Q52.Write a program to print the following pattern:
+
+*
+
+*
+*
+*
+
+*
+*
+*
+*
+*
+
+*
+*
+*
+
+*
+*/
+#include <stdio.h>
+
+int main() {
+    printf("*\n\n*\n*\n*\n\n*\n*\n*\n*\n*\n\n*\n*\n*\n\n*\n");
+    return 0;
+}
