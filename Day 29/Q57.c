64e24568e49a8736d2710f54fb0ea34f40724086
nidakhan-01 <nidakhan8459@gmail.com>
2025-10-24 17:25:41 +0530

    Add files via upload

---
+++ Day 29/Q57.c
@@ -0,0 +1,15 @@
+/*Q57.Find the sum of array elements.*/
+#include <stdio.h>
+int main() 
+{
+    int n, i, sum = 0;
+    printf("Enter size of array: ");
+    scanf("%d", &n);
+    int arr[n];
+    for(i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+        sum = sum + arr[i];
+    }
+    printf("%d", sum);
+    return 0;
+}
